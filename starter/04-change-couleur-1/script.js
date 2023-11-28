dc8ad8d97a898d59d4218bed3a6db2b05f27e015
tomcremer <tom.cremer@student.hepl.be>
2023-11-28 12:13:31 +0100

    Exo Completé

--- starter/04-change-couleur-1/script.js
+++ starter/04-change-couleur-1/script.js
@@ -6,7 +6,19 @@ EXERCICE 4 : Change couleur (1)
 Au clic sur un des boutons, colorie la page dans la couleur choisie par l'utilisateur.
 */
 
+document.getElementById("red").addEventListener("click", () => {
+    document.body.style.backgroundColor = "red";
+});
 
 
+document.getElementById("green").addEventListener("click", () => {
+    document.body.style.backgroundColor = "green";
+});
 
+document.getElementById("yellow").addEventListener("click", () => {
+    document.body.style.backgroundColor = "yellow";
+});
 
+document.getElementById("blue").addEventListener("click", () => {
+    document.body.style.backgroundColor = "blue";
+});
